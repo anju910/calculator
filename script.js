fdadc19a77d0d06aa8332d20c6868fb9d9758797
anju <anjukrishnanksd1998@gmail.com>
2021-06-22 09:25:19 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayValue(num){
+    let inputbox=document.getElementById("t1");
+    inputbox.value+=num;
+}
+
+function evaluateExpression(){
+    let cur_value=t1.value;
+    let res=eval(cur_value);
+    t1.value=res;
+}
+
+function clearBox(){
+    t1.value="";
+
+}
